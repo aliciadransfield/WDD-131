3f6f45e93d1e36d3a7d07b43006b3de5a3241964
aliciadransfield <alicia.m.dransfield@gmail.com>
2025-09-27 20:46:05 -0600

    Coolpics ready for turn in

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,20 +1,48 @@
+// Menu toggle
 const menuButton = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
 
 function toggleMenu() {
   menu.classList.toggle("hide");
 }
-
 menuButton.addEventListener("click", toggleMenu);
 
+// Resize handler
 function handleResize() {
-    const menu = document.querySelector(".menu");
-    if (window.innerWidth > 1000) {
-        menu.classList.remove("hide");
-    } else {
-        menu.classList.add("hide");
-    }
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
+}
+handleResize();
+window.addEventListener("resize", handleResize);
+
+// Viewer template function
+function viewHandler(event) {
+  const clicked = event.target;
+  if (clicked.tagName !== "IMG") return;
+
+  // swap small image with full image
+  const srcParts = clicked.getAttribute("src").split("-");
+  const newSrc = srcParts[0] + "-full.jpg";
+
+  const viewerTemplate = `
+    <div class="viewer">
+      <button class="close-viewer">X</button>
+      <img src="${newSrc}" alt="${clicked.alt}">
+    </div>`;
+
+  document.body.insertAdjacentHTML("afterbegin", viewerTemplate);
+
+  document.querySelector(".close-viewer")
+    .addEventListener("click", closeViewer);
 }
 
-handleResize();
-window.addEventListener("resize", handleResize)
+function closeViewer() {
+  const viewer = document.querySelector(".viewer");
+  viewer.remove();
+}
+
+// Attach click listener to gallery
+document.querySelector(".gallery").addEventListener("click", viewHandler);
